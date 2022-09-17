45b88bdc07804b7e357a45254702ebfc7dd735dc
vgeorge-fi <v.george@fi.com>
2022-09-16 21:09:57 -0700

    Build index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ try {
     //const payload = JSON.stringify(github.context.payload, undefined, 2)
     //console.log(`The event payload: ${payload}`);
 
-    console.log('github.context');
-    console.log(github.context);
+    console.log('github');
+    console.log(github);
 } catch (error) {
     core.setFailed(error.message);
 }
